a424fe1baf8fd9b859b362f03fa3704b683ea947
Carla Liarte Felipe <carla.lf7@gmail.com>
2022-02-13 09:14:52 +0100

    add listado de productos y funcionalidad de borrar

--- src/views/ProductosAdmin.vue
+++ src/views/ProductosAdmin.vue
@@ -42,12 +42,43 @@
       <div class="form-group">
         <button @click="guardarProd" class="btn btn-primary">Guardar</button>
       </div>
+
+      <hr />
+
+      <h3>Lista productos</h3>
+
+      <div class="table-responsive">
+
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Nombre</th>
+                      <th>Precio</th>
+                      <th>Modificar</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    <!-- se queda sin el :key porque si no da error-->
+                    <tr v-for="producto in productos">
+                      <td> {{producto.data().nombre}} </td>
+                      <td> {{producto.data().precio}} </td>
+                      <td>
+                        <button class="btn btn-primary">Editar</button>
+                        <button @click="borrarProducto(producto.id)" class="btn btn-danger">Borrar</button>
+                      </td>
+                    </tr>
+
+
+                  </tbody>
+                </table>
+            </div>
     </div>
   </div>
 </template>
 
 <script>
-import {firebase, db} from "../firebase.js";
+import { firebase, db, fb } from "../firebase.js";
 
 export default {
   name: "Productos",
@@ -56,30 +87,59 @@ export default {
   },
   data() {
     return {
+      productos: [],
       producto: {
         nombre: null,
-        precio: null
-      }
-    }
+        precio: null,
+      },
+    };
   },
   methods: {
-    guardarProd() {
-      db.collection("productos").add(this.producto)
-      .then((docRef) => {
-          console.log("Document written with ID: ", docRef.id);
-          this.reset();
-      })
-      .catch(function(error) {
-          console.error("Error adding document: ", error);
+     borrarProducto(doc){
+        if(confirm('¿Estas seguro de borrar este producto? ')){
+          db.collection("productos").doc(doc).delete().then(function() {
+              console.log("Producto borrado exitosamente!");
+          }).catch(function(error) {
+              console.error("Error al borrar el producto: ", error);
+          });
+          
+        }else{
+        }
+    },
+    leerDatos(){
+          db.collection("productos").get().then((querySnapshot) => {
+          // this.products = querySnapshot;
+          querySnapshot.forEach((doc) => {
+              // doc.data() is never undefined for query doc snapshots
+              this.productos.push(doc);
+          });
       });
     },
-    reset(){
-      Object.assign(this.$data, this.$options.data.apply(this));
-  }
-  }
+    guardarProd() {
+      db.collection("productos")
+        .add(this.producto)
+        .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+          this.reset();
+        })
+        .catch(function (error) {
+          console.error("Error adding document: ", error);
+        });
+    },
+    reset() {
+      // Object.assign(this.$data, this.$options.data.apply(this));
+    },
+     },
+  created(){
+    this.leerDatos();
+
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.form-group {
+  padding-bottom: 10px;
+}
 </style>
